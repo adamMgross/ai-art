71132180f050c756434b5e8c0e33511c6fe51346
adammgross <amgross4@gmail.com>
2017-02-19 23:36:15 +0000

    Scraped 675 images

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,6 +5,7 @@ import re
 from subprocess import call
 import sys
 from time import sleep
+import os.path
 
 DEBUG = False
 try:
@@ -21,24 +22,26 @@ def sh_exec(s):
 
 
 def parse_page(url, genre):
-    artist = url.split('/')[-1]
-    count = 0
-    fail_count = 0
-    for src in BeautifulSoup(urlopen(url)).findAll('img'):
-        if '%3A%2F%2F' in str(src):
-            #sleep(0.5)
-            link = extract_img_src(str(src))
-            try:
-                sh_exec('wget {} -O ./imgs/{}/{}-{}.jpg'
-                        .format(link,
-                                genre,
-                                artist,
-                                count))
-                count += 1
-            except:
-                fail_count += 1
-                print 'FAILED: {}\n\tfrom {}'.format(link, artist)
-    return (count, fail_count)
+    try:
+        artist = url.split('/')[-1]
+        count = 0
+        fail_count = 0
+        for src in BeautifulSoup(urlopen(url)).findAll('img'):
+            if '%3A%2F%2F' in str(src):
+                #sleep(0.5)
+                link = extract_img_src(str(src))
+                try:
+                    path = './imgs/{}/{}-{}.jpg'.format(genre, artist, count)
+                    if not os.path.isfile(path):
+                        sh_exec('wget {} -O {}'.format(link, path))
+                        count += 1
+                except:
+                    fail_count += 1
+                    print 'FAILED: {}\n\tfrom {}'.format(link, artist)
+        return (count, fail_count)
+    except:
+        print 'FAILED_URL: {}'.format(url)
+        sleep(10)
 
 
 def log(s):
@@ -60,13 +63,15 @@ def download_art_by_genre():
     total_num_success = 0
     total_num_failed = 0
     for artist in surreal_list:
+        if not artist:
+            continue
         num_success, num_failed = parse_page(artist, 'surrealist')
         total_num_success += num_success
         total_num_failed += num_failed
         sh_exec('clear')
         print('Finished {}'.format(artist.split('/')[-1]))
         print 'SUCCESS: {}\nFAILED: {}'.format(total_num_success, total_num_failed)
-        sleep(2)
+        #sleep(0.5)
     for artist in impression_list:
         num_success, num_failed = parse_page(artist, 'impressionist')
         total_num_success += num_success
