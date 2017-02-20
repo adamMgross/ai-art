6faac4154626810fafdee8bc65bfeae9759cc42d
Adam Gross <amgross4@gmail.com>
2017-02-20 12:35:05 -0600

    Update conduct_experiments.py

--- neural_network/src/conduct_experiments.py
+++ neural_network/src/conduct_experiments.py
@@ -17,34 +17,62 @@ def log(hidden_layers, hidden_layer_units, mini_batch_size, learning_rate):
     print 'MINI_BATCH_SIZE={}'.format(mini_batch_size)
     print 'LEARNING_RATE={}'.format(learning_rate)
 
+    
+def trial(hidden_layers, hidden_layer_units, learning_rate, mini_batch_size):
+    log(hidden_layers, hidden_layer_units, learning_rate, mini_batch_size)
+        topology = [784]
+        for i in range(hidden_layers):
+            topology.append(hidden_layer_units)
+        topology.append(10)
+        net = network.Network(topology)
+        t1 = time()
+        net.SGD(training_data,
+                30,
+                mini_batch_size,
+                learning_rate,
+                test_data)
+        t2 = time()
+        elapsed = t2-t1
+        print 'ELAPSED TIME: {} seconds'.format(elapsed)
+        print '====================================='
+        return elapsed
+    
 def run(training_data, test_data):
     hidden_layer_trials = [0, 1, 2]
     hidden_layer_units_trials = [10, 30, 50]
     learning_rate_trials = [0.01, 3, 30]
     mini_batch_size_trials = [1, 10, 100]
+    standard = [1, 30, 3.0, 10]
     total_time = 0
+    num_trials = 0
+    
     for hidden_layer_amt in hidden_layer_trials:
-        for hidden_layer_units_amt in hidden_layer_units_trials:
-            for learning_rate_amt in learning_rate_trials:
-                for mini_batch_size_amt in mini_batch_size_trials:
-                    log(hidden_layer_amt, hidden_layer_units_amt, mini_batch_size_amt, learning_rate_amt)
-                    topology = [784]
-                    for i in range(hidden_layer_amt):
-                        topology.append(hidden_layer_units_amt)
-                    topology.append(10)
-                    net = network.Network(topology)
-                    t1 = time()
-                    net.SGD(training_data,
-                            30,
-                            mini_batch_size_amt,
-                            learning_rate_amt,
-                            test_data)
-                    t2 = time()
-                    elapsed = (t2-t1)*1000.0
-                    print 'ELAPSED TIME: {}'.format(elapsed)
-                    total_time += elapsed
-                    print '====================================='
+        variables = standard[:]
+        variables[0] = hidden_layer_amt
+        total_time += trial(variables[0], variables[1], variables[2], variables[3])
+        num_trials += 1
+        
+    for hidden_layer_units_amt in hidden_layer_units_trials:
+        variables = standard[:]
+        variables[1] = hidden_layer_units_amt
+        total_time += trial(variables[0], variables[1], variables[2], variables[3])
+        num_trials += 1
+        
+    for learning_rate_amt in learning_rate_trials:
+        variables = standard[:]
+        variables[2] = learning_rate_amt
+        total_time += trial(variables[0], variables[1], variables[2], variables[3])
+        num_trials += 1
+        
+    for mini_batch_size_amt in mini_batch_size_trials:
+        variables = standard[:]
+        variables[3] = mini_batch_size_amt
+        total_time += trial(variables[0], variables[1], variables[2], variables[3])
+        num_trials += 1
+        
+    print '====================================='
     print 'TOTAL DURATION: {}'.format(total_time)
+    print 'NUM_TRIALS: {}'.format(num_trials)
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 run(training_data, test_data)
